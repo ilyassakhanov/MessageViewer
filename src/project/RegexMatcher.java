241072186bf6fee39c61b4812f8b31d03ffed9b5
Ilyas Sakhanov <ilyas.sakhanov@gmail.com>
2020-05-06 14:12:02 -0700

    Add an abstract class that contains methods for extracting data from the standalone message

---
+++ src/project/RegexMatcher.java
@@ -0,0 +1,28 @@
+package mypack;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+abstract class RegexMatcher {
+    String getInfo(String data, String regex) {
+
+        Pattern checkRegex = Pattern.compile(regex);
+        Matcher regexMatcher = checkRegex.matcher(data);
+        while (regexMatcher.find()) {
+            if (regexMatcher.group().length() != 0) {
+                return regexMatcher.group().trim();
+            }
+        }
+        return "The data was damaged";
+    }
+    String getInfoGroup(String data, String regex){
+        Pattern checkRegex = Pattern.compile(regex);
+        Matcher regexMatcher = checkRegex.matcher(data);
+        while (regexMatcher.find()) {
+            if (regexMatcher.group().length() != 0) {
+                return regexMatcher.group(1);
+            }
+        }
+        return "The data was damaged";
+    }
+}
