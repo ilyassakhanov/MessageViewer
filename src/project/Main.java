9b5733d1af8ff6acae7d11610f6ee8a89472200c
Ilyas Sakhanov <ilyas.sakhanov@gmail.com>
2020-05-07 02:21:56 -0700

    Change the title of the application, change name of .fxml file for more fitting one

--- src/project/Main.java
+++ src/project/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        Parent root = FXMLLoader.load(getClass().getResource("MessageViewer.fxml"));
+        primaryStage.setTitle("Viewer");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
