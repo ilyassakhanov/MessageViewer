1e6df4ad38863a0da5fc9ec3e543acdc851f83ab
Ilyas Sakhanov <ilyas.sakhanov@gmail.com>
2020-05-07 16:24:23 -0700

    Modify methods to work with RegexMatcher

--- src/project/Setter.java
+++ src/project/Setter.java
@@ -1,24 +1,26 @@
 package project;
 
 public class Setter extends RegexMatcher {
-    Message putTime(Message message, String data) {
+    Message putTime(Message message, String data) throws Exception {
         String time = getInfo(data, "\\d{2}:\\d{2}:\\d{2}");
         message.setTime(time);
         return message;
     }
 
-    Message putText(Message message, String data) {
+    Message putText(Message message, String data) throws Exception {
         String text = getInfoGroup(data, "Message:(.*)");
         message.setText(text);
         return message;
 
     }
-    Message putNick(Message message, String data){
-        String nickname = getInfoGroup(data, "Name:(.*)" );
+
+    Message putNick(Message message, String data) throws Exception {
+        String nickname = getInfoGroup(data, "Name:(.*)");
         message.setNick(nickname);
         return message;
     }
-    Message putMain (Message message, String data){
+
+    Message putMain(Message message, String data) throws Exception {
         message = putTime(message, data);
         message = putNick(message, data);
         message = putText(message, data);
