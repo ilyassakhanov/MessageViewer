1d50a709716b201f66cb83e7f1a826384550cc18
Ilyas Sakhanov <ilyas.sakhanov@gmail.com>
2020-05-07 22:40:19 -0700

    Add classes to the controller for select button, MessageGetter and buttons

--- src/project/Controller.java
+++ src/project/Controller.java
@@ -1,17 +1,35 @@
 package project;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+import javafx.scene.text.Text;
 import javafx.stage.FileChooser;
 
 import java.io.File;
+import java.util.ArrayList;
 
 public class Controller {
+    public Button selectFileButton;
+    public TextArea mainTextArea;
+    public Text pathText;
+
+
     public File onSelectFile(ActionEvent actionEvent) {
         FileChooser fc = new FileChooser();
+        FileToArrayList converter = new FileToArrayList();
         File selectedFile = fc.showOpenDialog(null);
+        MessageConstructor constructor = new MessageConstructor();
 
         if (selectedFile != null) {
-            return selectedFile.getAbsoluteFile();
+            try {
+                ArrayList<Message> test = converter.setMessages(selectedFile.getAbsoluteFile());
+                pathText.setText(selectedFile.getAbsolutePath());
+                System.out.println(constructor.constructMain(test.get(0)));
+            } catch (Exception e) {
+                Alert alert = new Alert(Alert.AlertType.ERROR, "The file has incorrect format!", ButtonType.OK);
+                alert.show();
+                pathText.setText("");
+            }
         }
         return null;
     }
