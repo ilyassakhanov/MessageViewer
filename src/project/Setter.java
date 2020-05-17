8202483d56e960c1f0aca9b38c1fbbb27440ac97
Ilyas Sakhanov <ilyas.sakhanov@gmail.com>
2020-05-16 17:57:42 -0700

    Add a functionality to remove names of senders if they repeat

--- src/project/Setter.java
+++ src/project/Setter.java
@@ -1,6 +1,8 @@
 package project;
 
 public class Setter extends RegexMatcher {
+
+
     Message putTime(Message message, String data) throws Exception {
         String time = getInfo(data, "\\d{2}:\\d{2}:\\d{2}");
         message.setTime(time);
@@ -14,15 +16,20 @@ public class Setter extends RegexMatcher {
 
     }
 
-    Message putNick(Message message, String data) throws Exception {
+    Message putNick(Message message, String data, String prevNick) throws Exception {
         String nickname = getInfoGroup(data, "Name:(.*)");
-        message.setNick(nickname);
-        return message;
+        if (nickname.equals(prevNick)) {
+            message.setNick("...");
+            return  message;
+        }else {
+            message.setNick(nickname);
+            return message;
+        }
     }
 
-    Message putMain(Message message, String data) throws Exception {
+    Message putMain(Message message, String data, String prevNick) throws Exception {
         message = putTime(message, data);
-        message = putNick(message, data);
+        message = putNick(message, data, prevNick);
         message = putText(message, data);
         return message;
     }
