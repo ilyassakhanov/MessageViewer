e0b6bd2d05b6bf984910693369c08b65b6905536
Ilyas Sakhanov <ilyas.sakhanov@gmail.com>
2020-05-16 17:58:36 -0700

    Add a functionality of storing a name of the previous message

--- src/project/FileToArrayList.java
+++ src/project/FileToArrayList.java
@@ -5,12 +5,16 @@ import java.util.ArrayList;
 
 public class FileToArrayList {
     ArrayList setMessages(File file) throws Exception {
+        String prevNick = "";
         ArrayList<Message> sortedMessages = new ArrayList<Message>();
         MessageGetter getter = new MessageGetter();
         ArrayList unsortedMessages = getter.getAll(file);
         Setter setter = new Setter();
         for (Object unsortedMessage : unsortedMessages) {
-            sortedMessages.add(setter.putMain(new Message(), (String) unsortedMessage));
+            sortedMessages.add(setter.putMain(new Message(), (String) unsortedMessage, prevNick));
+            if (!sortedMessages.get(sortedMessages.size() - 1).getNick().equals("...")) {
+                prevNick = sortedMessages.get(sortedMessages.size() - 1).getNick();
+            }
         }
         return sortedMessages;
     }
