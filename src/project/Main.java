ae8c3100fe779fa7b0a9eb902a0cb35fc7357af2
Ilyas Sakhanov <ilyas.sakhanov@gmail.com>
2020-05-26 20:26:16 -0700

    Change the name of a window from Viwer to Message Viewer

--- src/project/Main.java
+++ src/project/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("MessageViewer.fxml"));
-        primaryStage.setTitle("Viewer");
+        primaryStage.setTitle("Message Viewer");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
